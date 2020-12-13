66ae71ba077b3c48fa84407b8efb35107b9189b2
Alberto <alberto.r.caballero.87@gmail.com>
2020-12-13 12:08:15 +0100

    Definición de modelos comment y user

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'comment', 'user_id', 'post_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
